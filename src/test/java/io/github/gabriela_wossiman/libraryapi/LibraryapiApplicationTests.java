504a8df62b3caf8e587f6c918745aa6d087e264b
gabriela venancio <gabriela.v.valadao@gmail.com>
2025-12-12 08:42:29 -0300

    first

---
+++ src/test/java/io/github/gabriela_wossiman/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabriela_wossiman.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
